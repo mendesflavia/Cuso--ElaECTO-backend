8c5fd231890ba312c43656f4b4d6b73652a9f7f3
mendesflavia <mendesflavia@hotmail.com>
2021-02-09 09:51:05 -0300

    arrumando codigo no src/rotes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 //router.METHOD(CAMINHO), HANDLER)//
-router.get("/",(request, response)=>){
-response.send("Hello World")
+router.get("/",(_, response)=>{
+response.send('Hello World')
 });
 
 module.exports = router;
